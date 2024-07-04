c53ed77e9a73b37b071a132c6bf7cccfdd207d60
Sanaxen <bccatfalcon@gmail.com>
2024-07-04 22:35:33 +0900

    -

--- proj/test/blackhole_render/toGif/cnv.bat
+++ proj/test/blackhole_render/toGif/cnv.bat
@@ -1,4 +1,6 @@
-ffmpeg  -r 6 -y -i image%%d.bmp aaa.avi
-convert -geometry 45%% aaa.avi aaa.gif
+:ffmpeg  -r 6 -y -i image%%d.bmp aaa.mp4
+ffmpeg -r 6 -y -i image%%d.bmp -vcodec libx264 -pix_fmt yuv420p  aaa.mp4
+
+:convert -geometry 45%% aaa.avi aaa.gif
 
 
